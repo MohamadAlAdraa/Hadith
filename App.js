1cd3e65fa8eb0506c5010bfb26aa01df5ef83950
MohamadAlAdraa <mohamad.y.aladraa@gmail.com>
2023-05-25 19:00:43 -0400

    Add navigation container comp

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { StyleSheet } from "react-native";
 import { Provider } from "react-redux";
 import hadithStore from "./redux/index";
 import { createDrawerNavigator } from "@react-navigation/drawer";
+import { NavigationContainer } from '@react-navigation/native';
 import Search from "./screens/Search";
 import About from "./screens/About";
 
@@ -14,15 +15,17 @@ export default function App() {
     <>
       <StatusBar style="auto" />
       <Provider store={hadithStore}>
-        <Drawer.Navigator
-          initialRouteName="Search"
-          screenOptions={{
-            drawerPosition: "right",
-          }}
-        >
+        <NavigationContainer>
+          <Drawer.Navigator
+            initialRouteName="Search"
+            screenOptions={{
+              drawerPosition: "right",
+            }}
+          >
           <Drawer.Screen name="Search" component={Search} />
           <Drawer.Screen name="About" component={About} />
-        </Drawer.Navigator>
+          </Drawer.Navigator>
+          </NavigationContainer>
       </Provider>
     </>
   );

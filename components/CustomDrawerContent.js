f0a6ce3dcc0e7af6ac05864dadb5259bcadadc2c
Mohamad Al Adraa <mohamad.y.aladraa@gmail.com>
2023-05-26 15:55:57 -0400

    change drawer item and use the custom component

--- components/CustomDrawerContent.js
+++ components/CustomDrawerContent.js
@@ -1,82 +1,52 @@
 import React from "react";
-import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import { View, StyleSheet } from "react-native";
 import { DrawerContentScrollView } from "@react-navigation/drawer";
-import { MaterialCommunityIcons } from "@expo/vector-icons";
+import CustomDrawerItem from "./CustomDrawerItem";
+import colors from "../utils/colors";
 
 const CustomDrawerContent = (props) => {
-  const activeTabBackgroundColor = "#ddd"; // Customize the background color of the active tab
-
   const isTabActive = (routeName) => {
     const currentRoute = props.state.routes[props.state.index];
     return currentRoute.name === routeName;
   };
 
+  const onPressItemHandler = (routeName) => {
+    props.navigation.navigate(routeName);
+  };
+
+  const onPressShareHandler = (routeName) => {};
+
   return (
     <DrawerContentScrollView {...props}>
       <View style={styles.container}>
-        {/* Custom Drawer Items */}
-        <TouchableOpacity
-          style={[
-            styles.item,
-            isTabActive("Search") && {
-              backgroundColor: activeTabBackgroundColor,
-            },
-          ]}
-          onPress={() => props.navigation.navigate("Search")}
-        >
-          <MaterialCommunityIcons
-            name="book-open-page-variant"
-            size={24}
-            color="black"
-            style={styles.icon}
-          />
-          <Text>تحقق من الحديث</Text>
-        </TouchableOpacity>
-
-        <TouchableOpacity
-          style={[
-            styles.item,
-            isTabActive("About") && {
-              backgroundColor: activeTabBackgroundColor,
-            },
-          ]}
-          onPress={() => props.navigation.navigate("About")}
-        >
-          <MaterialCommunityIcons
-            name="information"
-            size={24}
-            color="black"
-            style={styles.icon}
-          />
-          <Text>عن التطبيق</Text>
-        </TouchableOpacity>
-
-        <TouchableOpacity
-          style={[
-            styles.item,
-            isTabActive("ContactUs") && {
-              backgroundColor: activeTabBackgroundColor,
-            },
-          ]}
-          onPress={() => props.navigation.navigate("ContactUs")}
-        >
-          <MaterialCommunityIcons
-            name="bug"
-            size={24}
-            color="black"
-            style={styles.icon}
-          />
-          <Text>أخبر عن خطأ</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.item}>
-          <MaterialCommunityIcons
-            name="share"
-            size={24}
-            color="black"
-            style={styles.icon}
-          />
-          <Text>مشاركة التطبيق</Text>
-        </TouchableOpacity>
+        <CustomDrawerItem
+          isTabActive={isTabActive}
+          tabName="Search"
+          onPress={onPressItemHandler}
+          iconLabel="book-open-page-variant"
+          label="تحقق من الحديث"
+        />
+        <CustomDrawerItem
+          isTabActive={isTabActive}
+          tabName="About"
+          onPress={onPressItemHandler}
+          iconLabel="information"
+          label="عن التطبيق"
+        />
+        <CustomDrawerItem
+          isTabActive={isTabActive}
+          tabName="ContactUs"
+          onPress={onPressItemHandler}
+          iconLabel="bug"
+          label="أخبر عن خطأ"
+        />
+        <CustomDrawerItem
+          isTabActive={isTabActive}
+          tabName="Share"
+          onPress={onPressShareHandler}
+          iconLabel="share"
+          label="مشاركة التطبيق"
+        />
       </View>
     </DrawerContentScrollView>
   );
@@ -89,19 +59,5 @@ const styles = StyleSheet.create({
     alignItems: "flex-end",
     flex: 1,
     marginTop: 20,
-  },
-  item: {
-    flexDirection: "row-reverse",
-    alignItems: "center",
-    padding: 10,
-    borderWidth: 1,
-    width: "94%",
-    marginVertical: 5,
-    alignSelf: "center",
-    borderRadius: 10,
-  },
-  icon: {
-    marginRight: 8,
-    marginLeft: 32,
   },
 });

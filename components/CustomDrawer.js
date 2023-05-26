56a69f12e159a1ad64a93a27d66af109fa10d940
Mohamad Al Adraa <mohamad.y.aladraa@gmail.com>
2023-05-26 01:18:58 -0400

    comment default content and use custom content component

--- components/CustomDrawer.js
+++ components/CustomDrawer.js
@@ -4,64 +4,82 @@ import { createDrawerNavigator } from "@react-navigation/drawer";
 import Search from "../screens/Search";
 import About from "../screens/About";
 import ContactUs from "../screens/ContactUs";
-import { MaterialCommunityIcons } from "@expo/vector-icons";
+// import { MaterialCommunityIcons } from "@expo/vector-icons";
+import CustomDrawerContent from "./CustomDrawerContent";
 
 const Drawer = createDrawerNavigator();
 
 const CustomDrawer = ({ size, iconColorOnFocus }) => {
   return (
+    // <Drawer.Navigator
+    //   initialRouteName="Search"
+    //   screenOptions={{
+    //     drawerPosition: "right",
+    //     drawerLabelStyle: {
+    //       textAlign: "right",
+    //     },
+    //     headerTitle: "",
+    //   }}
+    // >
+    //   <Drawer.Screen
+    //     name="Search"
+    //     component={Search}
+    //     options={{
+    //       drawerLabel: "تحقق من حديث",
+    //       drawerIcon: ({ focused }) => (
+    //         <MaterialCommunityIcons
+    //           name="book-open-page-variant"
+    //           size={size}
+    //           color={focused ? iconColorOnFocus : "black"}
+    //         />
+    //       ),
+    //     }}
+    //   />
+    //   <Drawer.Screen
+    //     name="ContactUs"
+    //     component={ContactUs}
+    //     options={{
+    //       drawerLabel: "أخبر عن خطأ",
+    //       drawerIcon: ({ focused }) => (
+    //         <MaterialCommunityIcons
+    //           name="bug"
+    //           size={size}
+    //           color={focused ? iconColorOnFocus : "black"}
+    //         />
+    //       ),
+    //     }}
+    //   />
+    //   <Drawer.Screen
+    //     name="About"
+    //     component={About}
+    //     options={{
+    //       drawerLabel: "عن التطبيق",
+    //       drawerIcon: ({ focused }) => (
+    //         <MaterialCommunityIcons
+    //           name="information"
+    //           size={size}
+    //           color={focused ? iconColorOnFocus : "black"}
+    //         />
+    //       ),
+    //     }}
+    //   />
+    // </Drawer.Navigator>
     <Drawer.Navigator
       initialRouteName="Search"
+      drawerContent={(props) => (
+        <CustomDrawerContent
+          {...props}
+          size={size}
+          iconColorOnFocus={iconColorOnFocus}
+        />
+      )}
       screenOptions={{
         drawerPosition: "right",
-        drawerLabelStyle: {
-          textAlign: "right",
-        },
-        headerTitle: "",
       }}
     >
-      <Drawer.Screen
-        name="Search"
-        component={Search}
-        options={{
-          drawerLabel: "تحقق من حديث",
-          drawerIcon: ({ focused }) => (
-            <MaterialCommunityIcons
-              name="book-open-page-variant"
-              size={size}
-              color={focused ? iconColorOnFocus : "black"}
-            />
-          ),
-        }}
-      />
-      <Drawer.Screen
-        name="ContactUs"
-        component={ContactUs}
-        options={{
-          drawerLabel: "أخبر عن خطأ",
-          drawerIcon: ({ focused }) => (
-            <MaterialCommunityIcons
-              name="bug"
-              size={size}
-              color={focused ? iconColorOnFocus : "black"}
-            />
-          ),
-        }}
-      />
-      <Drawer.Screen
-        name="About"
-        component={About}
-        options={{
-          drawerLabel: "عن التطبيق",
-          drawerIcon: ({ focused }) => (
-            <MaterialCommunityIcons
-              name="information"
-              size={size}
-              color={focused ? iconColorOnFocus : "black"}
-            />
-          ),
-        }}
-      />
+      <Drawer.Screen name="Search" component={Search} />
+      <Drawer.Screen name="About" component={About} />
+      <Drawer.Screen name="ContactUs" component={ContactUs} />
     </Drawer.Navigator>
   );
 };

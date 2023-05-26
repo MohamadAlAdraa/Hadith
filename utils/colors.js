2e2eb642dbed0e876ef2d9860c08e993dc21842e
Mohamad Al Adraa <mohamad.y.aladraa@gmail.com>
2023-05-26 15:54:29 -0400

    add central color file

---
+++ utils/colors.js
@@ -0,0 +1,10 @@
+const colors = {
+  main: "#31429B",
+  black: "black",
+  white: "white",
+  activeTabBackg: "#ddd",
+  activeTabIc: "#4756a0",
+  activeTabTex: "#4756a0",
+};
+
+export default colors;

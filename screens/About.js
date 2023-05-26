0b4e33659bc1ec6c07765016fac0fc77c4d63e2f
Mohamad Al Adraa <mohamad.y.aladraa@gmail.com>
2023-05-26 02:16:48 -0400

    simple page style

--- screens/About.js
+++ screens/About.js
@@ -3,7 +3,7 @@ import React from "react";
 
 const About = () => {
   return (
-    <View>
+    <View style={styles.container}>
       <Text>About</Text>
     </View>
   );
@@ -11,4 +11,10 @@ const About = () => {
 
 export default About;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});

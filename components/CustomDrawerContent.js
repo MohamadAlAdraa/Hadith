8243818d352501752548a44c5d030e4423ece4df
Mohamad Al Adraa <mohamad.y.aladraa@gmail.com>
2023-05-26 02:19:31 -0400

    Add styling and fix active tab. Item component to be made as more org step.

--- components/CustomDrawerContent.js
+++ components/CustomDrawerContent.js
@@ -1,49 +1,81 @@
 import React from "react";
-import { View, Text, TouchableOpacity } from "react-native";
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
 import { DrawerContentScrollView } from "@react-navigation/drawer";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 
-const CustomDrawerContent = ({ props, size, iconColorOnFocus }) => {
+const CustomDrawerContent = (props) => {
+  const activeTabBackgroundColor = "#ddd"; // Customize the background color of the active tab
+
+  const isTabActive = (routeName) => {
+    const currentRoute = props.state.routes[props.state.index];
+    return currentRoute.name === routeName;
+  };
+
   return (
     <DrawerContentScrollView {...props}>
-      <View>
+      <View style={styles.container}>
+        {/* Custom Drawer Items */}
         <TouchableOpacity
-          style={{ flexDirection: "row-reverse", alignItems: "center" }}
+          style={[
+            styles.item,
+            isTabActive("Search") && {
+              backgroundColor: activeTabBackgroundColor,
+            },
+          ]}
           onPress={() => props.navigation.navigate("Search")}
         >
           <MaterialCommunityIcons
             name="book-open-page-variant"
-            size={size}
+            size={24}
             color="black"
-            style={{ marginLeft: 16, marginRight: 8 }}
+            style={styles.icon}
           />
-          <Text style={{ marginRight: 16 }}>تحقق من حديث</Text>
+          <Text>تحقق من الحديث</Text>
         </TouchableOpacity>
 
         <TouchableOpacity
-          style={{ flexDirection: "row-reverse", alignItems: "center" }}
+          style={[
+            styles.item,
+            isTabActive("About") && {
+              backgroundColor: activeTabBackgroundColor,
+            },
+          ]}
           onPress={() => props.navigation.navigate("About")}
         >
           <MaterialCommunityIcons
             name="information"
-            size={size}
+            size={24}
             color="black"
-            style={{ marginLeft: 16, marginRight: 8 }}
+            style={styles.icon}
           />
-          <Text style={{ marginRight: 16 }}>من نحن</Text>
+          <Text>عن التطبيق</Text>
         </TouchableOpacity>
 
         <TouchableOpacity
-          style={{ flexDirection: "row-reverse", alignItems: "center" }}
+          style={[
+            styles.item,
+            isTabActive("ContactUs") && {
+              backgroundColor: activeTabBackgroundColor,
+            },
+          ]}
           onPress={() => props.navigation.navigate("ContactUs")}
         >
           <MaterialCommunityIcons
             name="bug"
-            size={size}
+            size={24}
             color="black"
-            style={{ marginLeft: 16, marginRight: 8 }}
+            style={styles.icon}
           />
-          <Text style={{ marginRight: 16 }}>أخبر عن خطأ</Text>
+          <Text>أخبر عن خطأ</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.item}>
+          <MaterialCommunityIcons
+            name="share"
+            size={24}
+            color="black"
+            style={styles.icon}
+          />
+          <Text>مشاركة التطبيق</Text>
         </TouchableOpacity>
       </View>
     </DrawerContentScrollView>
@@ -51,3 +83,25 @@ const CustomDrawerContent = ({ props, size, iconColorOnFocus }) => {
 };
 
 export default CustomDrawerContent;
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: "flex-end",
+    flex: 1,
+    marginTop: 20,
+  },
+  item: {
+    flexDirection: "row-reverse",
+    alignItems: "center",
+    padding: 10,
+    borderWidth: 1,
+    width: "94%",
+    marginVertical: 5,
+    alignSelf: "center",
+    borderRadius: 10,
+  },
+  icon: {
+    marginRight: 8,
+    marginLeft: 32,
+  },
+});

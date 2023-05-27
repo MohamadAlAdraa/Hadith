263f2fa5c70c4d6f2d847075e1e0eb08343c75b4
Mohamad Al Adraa <mohamad.y.aladraa@gmail.com>
2023-05-27 02:05:35 -0400

    move menuIcon to the right, add toggle drawer feature for logo and menuIcon, add theme Icon, add title page and centralize it

--- components/CustomDrawer.js
+++ components/CustomDrawer.js
@@ -5,26 +5,91 @@ import Search from "../screens/Search";
 import About from "../screens/About";
 import ContactUs from "../screens/ContactUs";
 import CustomDrawerContent from "./CustomDrawerContent";
+import HeaderButton from "./HeaderButton";
+import colors from "../utils/colors";
+import { useNavigation, DrawerActions } from "@react-navigation/native";
 
 const Drawer = createDrawerNavigator();
 
 const CustomDrawer = () => {
+  const navigation = useNavigation();
+
+  const openDrawerHandler = () => {
+    navigation.dispatch(DrawerActions.openDrawer());
+  };
+
+  const closeDrawerHandler = () => {
+    navigation.dispatch(DrawerActions.closeDrawer());
+  };
+
+  const toggleThemeHandler = () => {
+    // use redux state to change the card color and background
+  };
   return (
     <Drawer.Navigator
       initialRouteName="Search"
-      drawerContent={(props) => <CustomDrawerContent {...props} />}
+      drawerContent={(props) => (
+        <CustomDrawerContent
+          defaultProps={props}
+          onPress={closeDrawerHandler}
+        />
+      )}
       screenOptions={{
         drawerPosition: "right",
-        headerTitle: "",
+        headerTitleAlign: "center",
+        headerRight: () => (
+          <HeaderButton
+            onPress={openDrawerHandler}
+            style={styles.menuIcon}
+            iconLabel="menu"
+          />
+        ),
+        headerLeft: () => (
+          <HeaderButton
+            onPress={toggleThemeHandler}
+            style={styles.themeIcon}
+            iconLabel="theme-light-dark"
+          />
+        ),
+        headerTitleStyle: {
+          fontSize: 16,
+          color: colors.main,
+          fontWeight: "bold",
+        },
       }}
     >
-      <Drawer.Screen name="Search" component={Search} />
-      <Drawer.Screen name="About" component={About} />
-      <Drawer.Screen name="ContactUs" component={ContactUs} />
+      <Drawer.Screen
+        name="Search"
+        component={Search}
+        options={{
+          headerTitle: "الصفحة الرئيسية",
+        }}
+      />
+      <Drawer.Screen
+        name="About"
+        component={About}
+        options={{
+          headerTitle: "عن التطبيق",
+        }}
+      />
+      <Drawer.Screen
+        name="ContactUs"
+        component={ContactUs}
+        options={{
+          headerTitle: "أخبر عن خطأ",
+        }}
+      />
     </Drawer.Navigator>
   );
 };
 
 export default CustomDrawer;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  menuIcon: {
+    marginRight: 18,
+  },
+  themeIcon: {
+    marginLeft: 18,
+  },
+});

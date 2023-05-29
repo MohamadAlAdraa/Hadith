26052a7560b4cdda775ad2dc0d1cad958a000d5f
Mohamad Al Adraa <mohamad.y.aladraa@gmail.com>
2023-05-28 23:07:35 -0400

    Rope Knob connection between the hadith card and the social media container. SWEETY.

---
+++ components/HCardAndSMediaConnection.js
@@ -0,0 +1,35 @@
+import { StyleSheet, View } from "react-native";
+import React from "react";
+import colors from "../utils/colors";
+
+const HCardAndSMediaConnection = () => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.circle}></View>
+      <View style={styles.line}></View>
+    </View>
+  );
+};
+
+export default HCardAndSMediaConnection;
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  line: {
+    width: 2,
+    height: 20,
+    backgroundColor: colors.main,
+  },
+  circle: {
+    width: 10,
+    height: 10,
+    backgroundColor: colors.main,
+    borderRadius: 5,
+    borderWidth: 2,
+    borderColor: colors.main,
+  },
+});

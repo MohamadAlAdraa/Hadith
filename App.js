02ab3197bd9e85b8f7fff9e19449bf574b40e811
Mohamad Al Adraa <mohamad.y.aladraa@gmail.com>
2023-05-31 12:42:17 -0400

    Configure the cutom font to be loaded before rendering the App component. Splash screen will be loaded until the font is loaded

--- App.js
+++ App.js
@@ -1,26 +1,49 @@
 import "react-native-gesture-handler";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet } from "react-native";
+import { StyleSheet, View } from "react-native";
 import { Provider } from "react-redux";
 import hadithStore from "./redux/index";
 import { NavigationContainer } from "@react-navigation/native";
 import CustomDrawer from "./components/CustomDrawer";
 import { I18nManager } from "react-native";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import { useCallback } from "react";
+
+SplashScreen.preventAutoHideAsync();
 
 I18nManager.forceRTL(true);
 I18nManager.allowRTL(true);
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    "Amiri-Regular": require("./assets/fonts/Amiri-Regular.ttf"),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
-    <>
+    <View style={styles.container} onLayout={onLayoutRootView}>
       <StatusBar style="auto" />
       <Provider store={hadithStore}>
         <NavigationContainer>
           <CustomDrawer />
         </NavigationContainer>
       </Provider>
-    </>
+    </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});

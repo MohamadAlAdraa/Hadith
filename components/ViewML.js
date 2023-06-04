fd50897c4693b0dfca44c954fb4e9a75aff63001
Mohamad Al Adraa <mohamad.y.aladraa@gmail.com>
2023-06-04 15:51:59 -0400

    apply theme function

--- components/ViewML.js
+++ components/ViewML.js
@@ -2,12 +2,26 @@ import { StyleSheet, Text, TouchableOpacity } from "react-native";
 import React from "react";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import colors from "../utils/colors";
+import { useSelector } from "react-redux";
 
 const ViewML = ({ label, iconLabel, onPress }) => {
+  const themeState = useSelector((state) => state.theme.morning);
+
   return (
     <TouchableOpacity style={styles.container} onPress={onPress}>
-      <MaterialCommunityIcons name={iconLabel} size={20} color={colors.main} />
-      <Text style={styles.labelText}>{label}</Text>
+      <MaterialCommunityIcons
+        name={iconLabel}
+        size={20}
+        color={themeState ? colors.main : colors.neon}
+      />
+      <Text
+        style={[
+          styles.labelText,
+          { color: themeState ? colors.main : colors.neon },
+        ]}
+      >
+        {label}
+      </Text>
     </TouchableOpacity>
   );
 };
@@ -21,7 +35,7 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   labelText: {
-    fontWeight: "bold",
-    color: colors.main,
+    fontFamily: "Amiri-Bold",
+    // color: colors.main,
   },
 });

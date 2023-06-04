992e4252e0f870d75c2c48c47d0bbe04e4fd15d8
Mohamad Al Adraa <mohamad.y.aladraa@gmail.com>
2023-06-04 15:57:38 -0400

    Handle error API call

--- screens/Search.js
+++ screens/Search.js
@@ -1,43 +1,41 @@
-import {
-  ScrollView,
-  View,
-  Text,
-  StyleSheet,
-  ActivityIndicator,
-} from "react-native";
-import React from "react";
+import { ScrollView, View, StyleSheet, ActivityIndicator } from "react-native";
+import React, { useEffect } from "react";
 import BackgroundWrapper from "../components/BackgroundWrapper";
 import InputSearch from "../components/InputSearch";
 import HCardAndSMediaContainer from "../components/HCardAndSMediaContainer";
 import { useSelector } from "react-redux";
 import colors from "../utils/colors";
-import { generateUniqueKey } from "../utils/CONSTS";
+import { MESSAGE_TYPE, generateUniqueKey, showToast } from "../utils/CONSTS";
+import TasbihLoading from "../components/TasbihLoading";
+import InitialSearchPageContent from "../components/InitialSearchPageContent";
 
 const Search = () => {
   const hadithStateLoading = useSelector((state) => state.hadith.loading);
   const hadithStateData = useSelector((state) => state.hadith.data);
-  const hadithStateErro = useSelector((state) => state.hadith.error);
+  const hadithStateError = useSelector((state) => state.hadith.error);
+
+  useEffect(() => {
+    if (hadithStateError) {
+      showToast(MESSAGE_TYPE.ERROR, "حدث خطأ؛ تأكد من الاتصال بالانترنت.");
+    }
+  }, [hadithStateError]);
 
   return (
     <BackgroundWrapper>
       <ScrollView
-        contentContainerStyle={{
-          flexGrow: 1,
-          width: "100%",
-          marginTop: 30,
-          paddingBottom: 30,
-          alignItems: "center",
-          justifyContent: "flex-start",
-        }}
+        contentContainerStyle={styles.scrollViewContainer}
         showsVerticalScrollIndicator={false}
         keyboardShouldPersistTaps="handled"
       >
         <>
           <InputSearch />
           {hadithStateLoading ? (
-            <ActivityIndicator color={colors.main} size="large" />
+            <>
+              <ActivityIndicator color={colors.black} size="large" />
+              <TasbihLoading />
+            </>
           ) : hadithStateData ? (
-            <View style={{ width: "90%" }}>
+            <View style={styles.listContainer}>
               {hadithStateData.map((item, idx) => {
                 return (
                   <HCardAndSMediaContainer
@@ -48,29 +46,8 @@ const Search = () => {
                 );
               })}
             </View>
-          ) : hadithStateErro ? (
-            <View>
-              <Text>Error</Text>
-            </View>
           ) : (
-            <View style={styles.container}>
-              <Text style={styles.text}>
-                {`\u2022`}البحث متاح فقط باللغة العربية
-              </Text>
-              <Text style={styles.text}>
-                {`\u2022`}يمكنك البحث بكتابة بعض الكلمات من الحديث
-              </Text>
-              <Text style={styles.text}>
-                {`\u2022`}نتجية البحث تعرض :{" "}
-                <Text style={styles.innetText}>
-                  الروايات الواردة في كتب الحديث
-                </Text>{" "}
-                | <Text style={styles.innetText}>الراوي</Text> |{" "}
-                <Text style={styles.innetText}>المحدث</Text> |{" "}
-                <Text style={styles.innetText}>المصدر</Text> |{" "}
-                <Text style={styles.innetText}>خلاصة حكم الحديث</Text>
-              </Text>
-            </View>
+            <InitialSearchPageContent />
           )}
         </>
       </ScrollView>
@@ -81,27 +58,15 @@ const Search = () => {
 export default Search;
 
 const styles = StyleSheet.create({
-  container: {
+  scrollViewContainer: {
+    flexGrow: 1,
+    width: "100%",
+    marginTop: 30,
+    paddingBottom: 30,
+    alignItems: "center",
+    justifyContent: "flex-start",
+  },
+  listContainer: {
     width: "90%",
-    backgroundColor: colors.white,
-    borderRadius: 10,
-    shadowColor: colors.black,
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.3,
-    shadowRadius: 4,
-    elevation: 5,
-    paddingHorizontal: 15,
-    writingDirection: "rtl",
-    paddingVertical: 20,
-    borderWidth: 1,
-    borderColor: colors.main,
-  },
-  text: {
-    fontFamily: "Amiri-Bold",
-    fontSize: 20,
-    writingDirection: "rtl",
-  },
-  innetText: {
-    color: colors.golden,
   },
 });

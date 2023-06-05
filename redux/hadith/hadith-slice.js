0003962d4128e082cb04596f05f21901869a7446
Mohamad Al Adraa <mohamad.y.aladraa@gmail.com>
2023-06-04 21:54:07 -0400

    api url change

--- redux/hadith/hadith-slice.js
+++ redux/hadith/hadith-slice.js
@@ -7,7 +7,7 @@ export const SearchHadith_API = createAsyncThunk(
   async (userInput, { rejectWithValue }) => {
     try {
       const { data } = await axios.get(
-        APIS.baseURL + APIS.searchAPI + userInput
+        APIS.baseURL + APIS.searchAPI + userInput + "&t=*"
       );
       return data;
     } catch (error) {

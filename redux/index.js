3e5b3c706f99a2ac42515cb769b9d2ae553a182d
Mohamad Al Adraa <mohamad.y.aladraa@gmail.com>
2023-06-07 15:21:18 -0400

    Add theme and hadithexp reducers

--- redux/index.js
+++ redux/index.js
@@ -1,9 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import hadithSlice from "./hadith/hadith-slice";
 import themeSlice from "./theme/theme-slice";
+import sharehSlice from "./hadith/shareh-slice";
 
 const hadithStore = configureStore({
-  reducer: { hadith: hadithSlice.reducer, theme: themeSlice.reducer },
+  reducer: {
+    hadith: hadithSlice.reducer,
+    theme: themeSlice.reducer,
+    hadithExp: sharehSlice.reducer,
+  },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       immutableCheck: false,
